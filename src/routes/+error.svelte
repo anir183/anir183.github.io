bb8eb668f93b6e3f3453f51f32c76a2cddce4ecc
anir183 <188557785+anir183@users.noreply.github.com>
2026-05-15 23:52:25 +0530

    feat: add a basic error page
    wow

---
+++ src/routes/+error.svelte
@@ -0,0 +1,48 @@
+<script>
+	import { onMount } from "svelte";
+	import { gsap } from "gsap";
+	import { page } from '$app/state';
+
+	/** @type {HTMLHeadingElement} */
+	let title;
+
+	/** @type {HTMLParagraphElement} */
+	let subtitle;
+
+	onMount(() => {
+		const timeline = gsap.timeline();
+
+		timeline
+			.from(title, {
+				y: 80,
+				opacity: 0,
+				duration: 1,
+				ease: "power3.out"
+			})
+			.from(
+				subtitle,
+				{
+					y: 30,
+					opacity: 0,
+					duration: 0.8,
+					ease: "power2.out"
+				},
+				"-=0.5"
+			);
+	});
+</script>
+
+<section
+	class="flex min-h-screen flex-col items-center justify-center bg-zinc-950"
+>
+	<h1
+		bind:this={title}
+		class="text-7xl font-black tracking-tight text-zinc-100"
+	>
+		{page.status}
+	</h1>
+
+	<p bind:this={subtitle} class="mt-4 text-xl text-zinc-400">
+		{page.error ? page.error.message : "An Unknown Error Occured!"}
+	</p>
+</section>
